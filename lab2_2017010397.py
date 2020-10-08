f05f5fe7a6e789517da451b38cb90ec2eaf684dd
SteveEs25 <35873055+SteveEs25@users.noreply.github.com>
2020-10-08 01:07:20 -0600

    Agregando las estructuras de control
    Estructuras de control para examinar los numeros aleatorios

--- lab2_2017010397.py
+++ lab2_2017010397.py
@@ -7,3 +7,13 @@ sumatoria = 0
 
 num_aleatorio = np.random.normal(500, 30, 10000000)
 
+
+for numeros in num_aleatorio:
+	if int(numeros) <= 500000:
+		sumatoria = int(sumatoria) + int(numeros)
+
+
+
+print('La sumatoria de los puntos es: ' + str(sumatoria))
+print('Duracion: {} segundos'.format(time.time() - inicio))
+
