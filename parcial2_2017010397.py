5324c4e7b2d01bca1e0c36fbba1ff018646a9ece
SteveEs25 <35873055+SteveEs25@users.noreply.github.com>
2020-10-09 00:44:46 -0600

    Sumatoria de los datos
    Agregando la función para sumar los datos

--- parcial2_2017010397.py
+++ parcial2_2017010397.py
@@ -6,3 +6,11 @@ inicio = time.time()
 
 
 num_aleatorio = np.random.normal(500, 30, 10000000)
+
+
+sumatoria = np.sum(num_aleatorio, where = (num_aleatorio <= 500000))
+ 	
+
+
+print('La sumatoria de los puntos es: ' + str(sumatoria))
+print('Duracion: {} segundos'.format(time.time() - inicio))
